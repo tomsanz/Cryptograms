0a109ce2f81b290cc11cdd7e33b6b10aba5e0ea7
Tom Yin <huanxuyin@gmail.com>
2013-10-04 16:40:35 -0400

    Started a new project.

---
+++ src/Dave.scala
@@ -0,0 +1,12 @@
+object Dave {
+
+  def main(args: Array[String]): Unit = {
+    
+    def encode(plainText: String, code: String): String = ???
+    
+    
+    def decode(encodedText: String, code: String): String = ???
+    
+  }
+
+}
