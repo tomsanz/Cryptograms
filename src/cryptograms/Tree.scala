c1f51b1a5166cf0da1998e6691abb9d40f9426bb
Tom Yin <huanxuyin@gmail.com>
2013-10-08 09:15:42 -0400

    Updated Java version.

--- src/cryptograms/Tree.scala
+++ src/cryptograms/Tree.scala
@@ -1,12 +1,27 @@
 package cryptograms
 
-class CipherWord(val cipherW: String, val plainW: String, val score: Int)
+class CipherWord(val cipherW: String, val plainW: String, val score: Int) {
+  override def toString = "cipherWord is " + cipherW + ", and plain Word is: " + plainW
+}
 
-abstract class Node
+abstract class Node {
+  def include(c: CipherWord): Node
+  def isEmpty: Boolean
+}
 
-class EmptyNode extends Node {}
+class EmptyNode extends Node {
+  def include(c: CipherWord) = new NonEmptyNode(c, new EmptyNode)
+  def isEmpty = true
+}
 
-class Root extends Node {}
+class Root extends Node {
+  def isEmpty = false
+  def include(c: CipherWord): Node = ???
+}
 
-class NonEmptyNode() extends Node
+class NonEmptyNode(c: CipherWord, keys: Node*) extends Node {
+  def isEmpty = false
+  def include(c: CipherWord): Node = ???
+
+}
 
